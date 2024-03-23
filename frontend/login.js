23a3a1a1b0e478b9e194a4e262d9fd8286bd5b80
greenforet <lineon152@naver.com>
2024-03-23 10:48:14 +0900

    login flow 완성

--- frontend/login.js
+++ frontend/login.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#login-form");
 
 
+
 const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(form);
@@ -13,13 +14,11 @@ const handleSubmit = async (event) => {
         body:formData,
     });
     const data = await res.json();
+    const accessToken = data.access_token;
+    window.localStorage.setItem("token", accessToken);
+    alert("로그인되었습니다.");
 
-    if (res.status === '200'){
-        alert("로그인에 성공했습니다.");
-        window.location.pathname = "/";
-    }else if (res.status === 401){
-        alert("id 혹은 password가 틀렸습니다.");
-    }
+    window.location.pathname = "/";
 };
 
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile,Form,Response
+from fastapi import FastAPI, UploadFile,Form,Response,Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
@@ -16,11 +16,14 @@ SECRET = "super-coding"
 manager = LoginManager(SECRET,'/login')
 
 @manager.user_loader()
-def query_user(id):
+def query_user(data):
+    WHERE_STATEMENTS = f'id="{data}"'
+    if type(data) == dict:
+        WHERE_STATEMENTS = f'''id="{data['id']}"'''
     con.row_factory = sqlite3.Row
     cur = con.cursor()
     user = cur.execute(f"""
-                      SELECT * from users WHERE id = '{id}'
+                      SELECT * from users WHERE id = {WHERE_STATEMENTS}
                       """).fetchone()
     return user
 
@@ -34,9 +37,11 @@ def login(id:Annotated[str,Form()],
         raise InvalidCredentialsException
     
     access_token = manager.create_access_token(data={
-        'id':user['id'],
-        'name':user['name'],
-        'email':user['email']
+        'sub': {
+            'id':user['id'],
+            'name':user['name'],
+            'email':user['email']
+        }
     })
     return {'access_token':access_token}
                
@@ -77,7 +82,7 @@ async def create_item(image:UploadFile,
 
 
 @app.get('/items')
-async def get_items():
+async def get_items(user = Depends(manager)):
     #컬럼명도 같이 가져옴
     con.row_factory = sqlite3.Row
     cur = con.cursor()
